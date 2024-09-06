369d3ffa4535cbad1ffbc688ba61706ac0d66b4a
Hưng Vũ Khánh <vukhanhhung100@gmail.com>
2024-09-06 20:41:11 +0700

    Add new project Django9

--- NewProject/settings.py
+++ NewProject/settings.py
@@ -131,7 +131,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
